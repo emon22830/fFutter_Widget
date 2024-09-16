4122e71fef0206fff92c421c9b18d2792c95459b
emon22830 <mdemon22830@gmail.com>
2024-09-16 11:11:25 +0800

    Floating Action Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,12 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.green,
           title: Text('Flutter Widget'),
         ),
-
+        body: Center(
+          child: Text('Floating Action Button'),
+        ),
+        // floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.add,
+            color: Colors.red,
+          ),
+          backgroundColor: Colors.green,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(width: 5, color: Colors.yellow),
+            borderRadius: BorderRadius.all(Radius.circular(100)),
+          ),
+          onPressed: () {},
+          elevation: 40,
+          highlightElevation: 50,
+        ),
       ),
     );
   }
