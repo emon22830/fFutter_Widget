bc828872e924ad170c07b820c38546c32450df92
emon22830 <mdemon22830@gmail.com>
2024-09-18 09:52:35 +0800

    Fitted Box

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,14 +13,14 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FittedBox(
-        child: Text(
-          'This is a simple text brother , whats app man',
-          style: TextStyle(
-            fontSize: 60,
-          ),
+      body:FittedBox(
+        child: Row(
+          children: [
+            Image.network('https://up.yimg.com/ib/th?id=OIP.G37tgeQqSNt7v2oPfj9ltQAAAA&pid=Api&rs=1&c=1&qlt=95&w=146&h=97'),
+            Image.network('https://up.yimg.com/ib/th?id=OIP.zsxVgLLWH6VF-CZcpC49MwHaFj&pid=Api&rs=1&c=1&qlt=95&w=132&h=99'),
+          ],
         ),
-      ),
+      )
     );
   }
 }
